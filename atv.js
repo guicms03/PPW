d3f67a84a3e2adf169671f29c524e41f21015f98
luisgustavoos <anacarladocumentos@gmail.com>
2024-03-01 19:52:36 -0300

    aula

---
+++ atv.js
@@ -0,0 +1,41 @@
+
+var numero1 = Number(prompt("Digite o primeiro número:"));
+
+
+var numero2 = Number(prompt("Digite o segundo número:"));
+
+
+var numero3 = Number(prompt("Digite o terceiro número:"));
+
+
+console.log("Os números digitados são:");
+console.log("Número 1: " + numero1);
+console.log("Número 2: " + numero2);
+
+
+var numero1 = Number(prompt("Digite o primeiro número:"));
+
+
+var numero2 = Number(prompt("Digite o segundo número:"));
+
+
+var numero3 = Number(prompt("Digite o terceiro número:"));
+
+
+var soma = numero1 + numero2 + numero3;
+
+
+var media = soma / 3;
+
+
+console.log("A média dos números digitados é: " + media);
+
+
+var numero1 = 10;
+var numero2 = 20;
+var numero3 = 30;
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
